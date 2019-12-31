1f06df4d7efe63b76bf53a5e04b84b8bb4ea97ed
3520097 <apolline.gruaux@etu.upmc.fr>
2019-12-31 12:17:15 +0100

    création de la fenetre, bouton close,

--- main.cc
+++ main.cc
@@ -1,5 +1,26 @@
 #include "Snake.hh"
+#include <SFML/Window.hpp>
 
 int main(){
-  Snake a;
+	// taille , nom , titre affiché, bouton fermé , pas resizeable , le type renderWindow permet de dessiner dans la fenetre
+sf::RenderWindow window(sf::VideoMode(1071,1071), "Snake 4.0",sf::Style::Titlebar | sf::Style::Close);
+ while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))	
+        {
+        	 Snake a;
+            // "close requested"
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+         window.display();
+    }
+
+
+
+ 
+  return 0;
 }
