6d4feaaaf796e8a9792e7ec7dfe6a49940adeaad
3520097 <apolline.gruaux@etu.upmc.fr>
2020-01-10 18:04:24 +0100

    test texture fond

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=g++
 CCFLAGS= -Wall -Werror -std=c++11 -g
-LIBFLAGS=-lsfml-graphics -lsfml-window -lsfml-system
+LIBFLAGS=
 SRC= $(wildcard *.cc)
 OBJ= $(SRC:.cc=.o)
 OBJ_TEST = $(filter-out logic.o, $(OBJ)) $(TST:.cc=.o)
@@ -13,13 +13,13 @@ testcase : $(OBJ_TEST)
 	g++ -std=c++11 -Wall -g  -o $@ $^
 
 $(EXEC): $(OBJ)
-	$(CC) $(LIBFLAGS) $^ -o $@
+	$(CC) $(LIBFLAGS) $^ -o $@ -lsfml-graphics -lsfml-window -lsfml-system
 
 %.o: %.cc
 	$(CC) $(CCFLAGS) -o $@ -c $<
 
 .depend:
-
+	
 	g++ -MM $(SRC) > .depends
 -include .depends
 
--- main.cc
+++ main.cc
@@ -4,22 +4,35 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-	// taille , nom , titre affiché, bouton fermé , pas resizeable , le type renderWindow permet de dessiner dans la fenetre
-sf::Window fenetre(sf::VideoMode(1071,1071), "Snake 4.0",sf::Style::Titlebar | sf::Style::Close);
+	// taille , nom , titre affiché, bouton fermé , pas resizeable , le type renderWindow permet de dessiner dans la window
+sf::RenderWindow window(sf::VideoMode(1071,1071), "Snake 4.0",sf::Style::Titlebar | sf::Style::Close);
 
- while (fenetre.isOpen())
+// FOND VERT
+sf::Texture fondTexture;
+    if (!fondTexture.loadFromFile(images/"chemin.png"))
+        return EXIT_FAILURE;
+    sf::Sprite fond(fondTexture);
+
+// test mur
+    sf::Texture textureMur;
+    if(!textureMur.loadFromFile("mur.jpg", sf::IntRect(200, 400, 32, 60))){
+        return EXIT_FAILURE;
+    }
+    sf::Sprite spriteMur(textureMur);
+
+ while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (fenetre.pollEvent(event))	
+        while (window.pollEvent(event))	
         {
         	 Snake a;
             // "close requested"
             if (event.type == sf::Event::Closed)
-                fenetre.close();
+                window.close();
         }
 
-         fenetre.display();
+         window.display();
     }
 
 
