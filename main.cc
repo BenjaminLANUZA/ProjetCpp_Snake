0edf3dc067db59b379b86ebe722d7b5c3b8b6f8d
3520097 <apolline.gruaux@etu.upmc.fr>
2020-01-17 18:15:31 +0100

    aj thread

--- main.cc
+++ main.cc
@@ -3,37 +3,51 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-int main(){
-	// taille , nom , titre affiché, bouton fermé , pas resizeable , le type renderWindow permet de dessiner dans la window
-sf::RenderWindow window(sf::VideoMode(1071,1071), "Snake 4.0",sf::Style::Titlebar | sf::Style::Close);
 
-// FOND VERT
-sf::Texture fondTexture;
-    if (!fondTexture.loadFromFile(images/"chemin.png"))
-        return EXIT_FAILURE;
-    sf::Sprite fond(fondTexture);
+const int 
 
-// test mur
-    sf::Texture textureMur;
-    if(!textureMur.loadFromFile("mur.jpg", sf::IntRect(200, 400, 32, 60))){
+void mainFunction(){
+
+    Snake  a;
+}
+
+void mursContour(){
+
+      sf::Texture textureMur;
+
+    if(!textureMur.loadFromFile("images/Murs/Mur_Normal.png", sf::IntRect(200, 400, 32, 60))){
         return EXIT_FAILURE;
     }
     sf::Sprite spriteMur(textureMur);
 
- while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))	
-        {
-        	 Snake a;
-            // "close requested"
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+}
 
-         window.display();
-    }
+int main(){
+    //STATIC GRAPHICS
+
+    // taille , nom , titre affiché, bouton fermé , pas resizeable , le type renderWindow permet de dessiner dans la window
+sf::RenderWindow window(sf::VideoMode(1071,1071), "Snake 4.0",sf::Style::Titlebar | sf::Style::Close);
+
+// FOND VERT
+sf::Texture fondTexture;
+    if (!fondTexture.loadFromFile("images/Chemin.jpg"))
+        return EXIT_FAILURE;
+    sf::Sprite fond(fondTexture);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+ // create a thread with func() as entry point
+    sf::Thread thread(&mainFunction);
+// lance le snake
+    thread.launch();
+
+   
+
+   
+
+
 
 
 
