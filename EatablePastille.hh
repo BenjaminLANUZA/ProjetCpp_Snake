b213c06355c3b98483d9493ed8693951f3629d60
3520097 <apolline.gruaux@etu.upmc.fr>
2020-01-19 19:58:03 +0100

    presque la.2

--- EatablePastille.hh
+++ EatablePastille.hh
@@ -8,28 +8,26 @@ public:
         cout<<"construct pastille eatable"<<endl; 
       (this->texture).loadFromFile("images/Pastilles/Eatable_Pastille.png", sf::IntRect(0, 0, 63, 63));
     (this->sprite).setTexture(this->texture);
-        (this->sprite).setPosition(500.f,500.f);
-        std::cout<<"connar défault"<< std::endl;
+        (this->sprite).setPosition((63.f+63.f),(63.f+63.f));
 
   };
   EatablePastille(int positionX, int positionY):Pastille(positionX, positionY){
     cout<<"construct pastille eatable"<<endl; 
       (this->texture).loadFromFile("images/Pastilles/Eatable_Pastille.png", sf::IntRect(0, 0, 63, 63));
     (this->sprite).setTexture(this->texture);
-    (this->sprite).setPosition(500.f,500.f);
+    (this->sprite).setPosition(positionX*(63.f+63.f),positionY*(63.f+63.f));
   }
   EatablePastille(const EatablePastille& e):Pastille(e.getX(),e.getY()){
         cout<<"construct pastille eatable"<<endl; 
       (this->texture).loadFromFile("images/Pastilles/Eatable_Pastille.png", sf::IntRect(0, 0, 63, 63));
     (this->sprite).setTexture(this->texture);
-    (this->sprite).setPosition(500.f,500.f);
+    (this->sprite).setPosition(e.getX()*(63.f+63.f),e.getY()*(63.f+63.f));
 
   }
   EatablePastille(const Chemin& c):Pastille(c){
      (this->texture).loadFromFile("images/Pastilles/Eatable_Pastille.png", sf::IntRect(0, 0, 63, 63));
     (this->sprite).setTexture(this->texture);
-        (this->sprite).setPosition(500.f,500.f);
-          std::cout<<"connar chemin"<< std::endl;
+        (this->sprite).setPosition((63.f+63.f),(63.f+63.f));
 
   }
   ~EatablePastille(){}
